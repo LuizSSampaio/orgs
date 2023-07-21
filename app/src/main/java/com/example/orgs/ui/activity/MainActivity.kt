4b0903bd96cc787045f7e66270c78a2390c00232
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-20 21:55:14 -0300

    :rocket: feat: Added the screen to create a new item

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.model.Products
 import com.example.orgs.ui.recyclerView.adapter.ProductListAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
