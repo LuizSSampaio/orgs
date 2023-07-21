5cb3680420394db53e1eddd03e1dab348d7c80dd
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-21 09:02:46 -0300

    :rocket: feat: Get data from the form

--- app/src/main/java/com/example/orgs/model/Products.kt
+++ app/src/main/java/com/example/orgs/model/Products.kt
@@ -2,7 +2,7 @@ package com.example.orgs.model
 
 import java.math.BigDecimal
 
-class Products(
+data class Products(
     var name: String,
     var description: String,
     var price: BigDecimal
--- app/src/main/java/com/example/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ProductFormActivity.kt
@@ -2,11 +2,36 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.example.orgs.R
+import com.example.orgs.model.Products
+import java.math.BigDecimal
 
 class ProductFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_form)
+
+        val nameField = findViewById<EditText>(R.id.editName)
+        val descriptionField = findViewById<EditText>(R.id.editDescription)
+        val priceField = findViewById<EditText>(R.id.editPrice)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        saveButton.setOnClickListener {
+            val priceString = priceField.text.toString()
+            val price = if (priceString.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(priceString.toString())
+            }
+
+            val newProduct = Products(
+                nameField.text.toString(),
+                descriptionField.text.toString(),
+                price
+            )
+        }
     }
 }
